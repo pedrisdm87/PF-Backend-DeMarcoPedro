2dd6ce75f8d7e22b0329a93f88126c3ac945fbef
PEDRO DE MARCO <bordyestamp@gmail.com>
2024-02-05 12:07:37 -0300

    mensajes de error en userPasswordDao

--- src/dao/userPassword.dao.js
+++ src/dao/userPassword.dao.js
@@ -1,4 +1,4 @@
-import UserPasswordModel from "../dao/models/user-password.model.js";
+/*import UserPasswordModel from "../dao/models/user-password.model.js";
 
 const userPasswordDAO = {
   create: async (email, token) => {
@@ -13,6 +13,40 @@ const userPasswordDAO = {
 
   deleteOne: async (id) => {
     const updateTicket = await UserPasswordModel.deleteOne(id);
+  },
+};
+
+export default userPasswordDAO;
+*/
+
+import UserPasswordModel from "../dao/models/user-password.model.js";
+
+const userPasswordDAO = {
+  create: async (email, token) => {
+    try {
+      const createToken = await UserPasswordModel.create({ email, token });
+      return createToken;
+    } catch (err) {
+      throw new Error("Error al crear el token de usuario en la base de datos");
+    }
+  },
+
+  findOne: async (token) => {
+    try {
+      const user = await UserPasswordModel.findOne({ token });
+      return user;
+    } catch (err) {
+      throw new Error("Error al buscar el token de usuario en la base de datos");
+    }
+  },
+
+  deleteOne: async (id) => {
+    try {
+      const updateToken = await UserPasswordModel.deleteOne({ _id: id });
+      return updateToken;
+    } catch (err) {
+      throw new Error("Error al eliminar el token de usuario de la base de datos");
+    }
   },
 };
 
