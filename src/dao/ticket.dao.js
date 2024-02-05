2216e410d5375f9d7f9884b71e63f6663603224a
PEDRO DE MARCO <bordyestamp@gmail.com>
2024-02-05 12:02:25 -0300

    mensajes de error en ticketDao

--- src/dao/ticket.dao.js
+++ src/dao/ticket.dao.js
@@ -1,4 +1,4 @@
-import ticketModel from "../dao/models/ticket.model.js";
+/*import ticketModel from "../dao/models/ticket.model.js";
 
 const ticketDAO = {
   findById: async (id) => {
@@ -21,6 +21,48 @@ const ticketDAO = {
       return ticket;
     } catch (err) {
       return err;
+    }
+  },
+};
+
+export default ticketDAO;
+*/
+import ticketModel from "../dao/models/ticket.model.js";
+
+const ticketDAO = {
+  findById: async (id) => {
+    try {
+      const getById = await ticketModel.findById(id);
+      return getById;
+    } catch (err) {
+      throw new Error("Error al buscar el ticket por ID");
+    }
+  },
+
+  create: async (ticket) => {
+    try {
+      const createTicket = await ticketModel.create(ticket);
+      return createTicket;
+    } catch (err) {
+      throw new Error("Error al crear un nuevo ticket");
+    }
+  },
+
+  updateTicket: async (id) => {
+    try {
+      const updateTicket = await ticketModel.update(id);
+      return updateTicket;
+    } catch (err) {
+      throw new Error("Error al actualizar el ticket");
+    }
+  },
+
+  saveTicket: async () => {
+    try {
+      const ticket = await ticketModel.find();
+      return ticket;
+    } catch (err) {
+      throw new Error("Error al guardar el ticket");
     }
   },
 };
